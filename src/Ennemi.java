d85b46d0e9543ebac351578d68b8f260491fcdac
Noemie <noemie.clay@hotmail.fr>
2016-10-25 15:11:07 +0200

    Ajout des ennemis

---
+++ src/Ennemi.java
@@ -0,0 +1,36 @@
+import MG2D.*;
+import MG2D.geometrie.*;
+
+public class Ennemi extends Texture {
+
+	public Ennemi() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Ennemi(Couleur couleur, String chemin, Point a, int larg, int haut) {
+		super(couleur, chemin, a, larg, haut);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Ennemi(Couleur couleur, String chemin, Point a) {
+		super(couleur, chemin, a);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Ennemi(String chemin, Point a, int larg, int haut) {
+		super(chemin, a, larg, haut);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Ennemi(String chemin, Point a) {
+		super(chemin, a);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Ennemi(Texture t) {
+		super(t);
+		// TODO Auto-generated constructor stub
+	}
+
+}
